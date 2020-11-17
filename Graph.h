2a5a2862877583fe009fb0a3f08d60af5d97d0ee
kfirs127 <62557231+kfirs127@users.noreply.github.com>
2020-11-17 21:58:47 +0200

    new classes

---
+++ Graph.h
@@ -0,0 +1,20 @@
+#ifndef GRAPH_H_
+#define GRAPH_H_
+#include <vector>
+
+class Graph{
+public:
+    Graph();
+    Graph(std::vector<std::vector<int>> matrix);
+
+    void infectNode(int nodeInd);
+    bool isInfected(int nodeInd);
+    Graph BFS(Graph G , int sorce);
+    std::vector<std::vector<int>> GetEdges();
+
+private:
+    std::vector<std::vector<int>> edges;
+    std::vector<bool> Inodes;  // will represent the infected nodes.
+}
+
+#endif
---
+++ Session.h
@@ -0,0 +1,47 @@
+#ifndef UNTITLED_SESSION_H
+#define UNTITLED_SESSION_H
+
+#endif //UNTITLED_SESSION_H
+#ifndef SESSION_H_
+#define SESSION_H_
+
+#include <vector>
+#include <string>
+#include <queue>
+#include "Graph.h"
+
+class Agent;
+
+enum TreeType{
+    Cycle,
+    MaxRank,
+    Root
+};
+
+class Session{
+public:
+    Session(const std::string& path);
+    virtual ~Session();
+    Session(const  Session &other);
+    const Session& operator=(const Session &other);
+    Session(Session&& other);
+    const Session& operator=(Session&& other);
+
+    void simulate();
+    void addAgent(const Agent& agent);
+    void setGraph(const Graph& graph);
+
+    void enqueueInfected(int Vnode);
+    int dequeueInfected();
+    TreeType getTreeType() const;
+
+    std::vector<std::vector<int>> getEdges();
+
+private:
+    Graph g;
+    TreeType treeType;
+    std::vector<Agent*>agents;
+    std::queue<int> infected;
+};
+
+#endif
